2ea9a6cf436a4c3de250b7e0e412938fa6b52525
Maya <mayahm01@gmail.com>
2018-03-17 21:56:37 +0300

    реализация открытия мобильного меню, добавлены новые svg иконки, новые стили по styleguide

---
+++ source/js/main.js
@@ -0,0 +1,19 @@
+'use strict';
+var navButton = document.querySelector('.navigation-toggle');
+var navigation = document.querySelector('.navigation');
+
+
+navigation.classList.remove('navigation--nojs');
+navButton.addEventListener('click', function (evt) {
+    if (navigation.classList.contains('navigation--closed')) {
+        navigation.classList.remove('navigation--closed');
+        navigation.classList.add('navigation--open');
+        navButton.classList.remove('navigation-toggle--open');
+        navButton.classList.add('navigation-toggle--close');
+    } else {
+        navigation.classList.remove('navigation--open');
+        navButton.classList.remove('navigation-toggle--close');
+        navigation.classList.add('navigation--closed');
+        navButton.classList.add('navigation-toggle--open');
+    }
+});
