2d2f5a2d7e9f9ed7307d403ef8d858d5ae889640
Maya <mayahm01@gmail.com>
2018-04-08 12:50:49 +0300

    новые стили для блока живой пример

--- source/js/main.js
+++ source/js/main.js
@@ -1,19 +1,20 @@
 'use strict';
 var navButton = document.querySelector('.navigation-toggle');
 var navigation = document.querySelector('.navigation');
+var navigationItem = document.querySelectorAll('.navigation__item');
 
 
 navigation.classList.remove('navigation--nojs');
 navButton.addEventListener('click', function (evt) {
-    if (navigation.classList.contains('navigation--closed')) {
-        navigation.classList.remove('navigation--closed');
-        navigation.classList.add('navigation--open');
-        navButton.classList.remove('navigation-toggle--open');
-        navButton.classList.add('navigation-toggle--close');
-    } else {
-        navigation.classList.remove('navigation--open');
-        navButton.classList.remove('navigation-toggle--close');
-        navigation.classList.add('navigation--closed');
-        navButton.classList.add('navigation-toggle--open');
-    }
+  if (navigation.classList.contains('navigation--closed')) {
+    navigation.classList.remove('navigation--closed');
+    navigation.classList.add('navigation--open');
+    navButton.classList.remove('navigation-toggle--open');
+    navButton.classList.add('navigation-toggle--close');
+  } else {
+    navigation.classList.remove('navigation--open');
+    navButton.classList.remove('navigation-toggle--close');
+    navigation.classList.add('navigation--closed');
+    navButton.classList.add('navigation-toggle--open');
+  }
 });
